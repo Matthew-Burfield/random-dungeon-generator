b904a468fc63719df78f05e6ab8babb2d478a830
Matthew-Burfield <m1.burfield@gmail.com>
2017-03-26 21:18:47 +1000

    added init and fixed bug where dungeon couldn't reference own properties

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const Dungeon = {
     a: "a",
     b: "b",
     c: "c",
+    init(a) {
+      this.a = a;
+    },
     abd() {
-        return a + b + c;
+        return this.a + this.b + this.c;
     },
 };
 
-function NewDungeon() {
-  return Object.assign(Dungeon);
+function NewDungeon(a = "a") {
+  const dungeon = Object.assign(Dungeon);
+  dungeon.init(a);
+  return dungeon;
 }
 
 module.exports = NewDungeon;
